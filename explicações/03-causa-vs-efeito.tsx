b3c3b5c6d69870af8b69e3a90ca547bf66bdc627
Giovanna Linda <eugiovannasouza@gmail.com>
2022-12-05 10:43:17 -0300

    feat: cause vs effect

---
+++ explicações/03-causa-vs-efeito.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function Button() {
+  const isFormSubmitting = true
+
+  return (
+    <button disabled={isFormSubmitting}>
+      <span></span>
+      {isFormSubmitting ? 'Carregando' : 'Enviar'}
+    </button>
+  )
+}
