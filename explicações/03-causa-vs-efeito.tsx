8b420879101a0c5b564454da010c955e93762626
Giovanna Linda <eugiovannasouza@gmail.com>
2022-12-07 21:42:21 -0300

    feat: cause vs effect

--- explicações/03-causa-vs-efeito.tsx
+++ explicações/03-causa-vs-efeito.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
+// Sempre nomear as variáveis pela causa e nunca pelo efeito
+
+// ❌ errado
+// efeito
+export function Button() {
+  const isButtonDisabled = true
+
+  return (
+    <button disabled={isButtonDisabled}>
+      <span></span>
+      {isButtonDisabled ? 'Carregando' : 'Enviar'}
+    </button>
+  )
+}
+
+// ✅ certo
+// causa
 export function Button() {
   const isFormSubmitting = true
 
